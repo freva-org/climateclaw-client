1fd8a72ee72ca1892a4af20135a98e7f82e13335
Felix Oertel <oertel@dkrz.de>
2026-05-10 11:44:16 +0200

    add setup.py file to project

---
+++ setup.py
@@ -0,0 +1,9 @@
+import versioneer
+from setuptools import find_packages, setup
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+)
