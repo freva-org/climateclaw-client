97d64c48ba6f84b154e903e0d6b61d471ac3c803
Felix Oertel <oertel@dkrz.de>
2026-05-13 23:14:00 +0200

    revert hardcoded version, add client and models imports to __all__

--- src/freva_gpt_client/__init__.py
+++ src/freva_gpt_client/__init__.py
@@ -1,1 +1,29 @@
-__version__ = "0.0.1"
+from ._version import get_versions
+from .client import AsyncFrevaGPT, FrevaGPT
+from .models import (
+    Assistant,
+    Code,
+    CodeOutput,
+    Conversation,
+    Image,
+    Prompt,
+    StreamConversation,
+    User,
+)
+
+__version__ = get_versions()["version"]
+__all__ = [
+    "AsyncFrevaGPT",
+    "FrevaGPT",
+    "Assistant",
+    "Code",
+    "CodeOutput",
+    "Conversation",
+    "Image",
+    "Prompt",
+    "StreamConversation",
+    "User",
+    "__version__",
+]
+
+del get_versions
