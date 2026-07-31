ec972202905d900abfe270234e954f50b39933d9
Felix Oertel <oertel@dkrz.de>
2026-07-31 17:19:58 +0200

    replace flit backend with setuptools for compatibility with versioneer

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import find_packages, setup
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages("src"),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
+    package_data={"climate_claw_client": ["py.typed"]},
+    zip_safe=False,
 )
